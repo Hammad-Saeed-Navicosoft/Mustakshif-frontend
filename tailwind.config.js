7c4d6f3ee5ca3e54f340595bdf7044308e2830a2
Hammad-Saeed-Navicosoft <hammad.navicosoft@gmail.com>
2025-09-19 11:14:08 +0500

    homepage adjustment

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,11 @@ module.exports = {
         bagoss: ["BagossStandardTRIAL-Regular", "sans-serif"],
         sen: ["Sen", "sans-serif"],
       },
+      screens: {
+        '3xl': '1720px',
+        '4xl': '1840px',
+        '5xl': '1900px',
+      },
     },
   },
   plugins: [
